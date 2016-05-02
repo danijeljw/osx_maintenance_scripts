47343824c3070767e0820ff35c850f7521800fd4
Danijel James <danijel.james@me.com>
2016-05-02 23:37:56 +0930

    dyld clearing cache script

---
+++ clear_dyld_cache.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+##################################################
+# Name: clear_dyld_cache.sh
+# Description: Clear the dyld Cache
+# Script Maintainer: Danijel-James Wynyard
+#
+# Last Updated: May 2, 2016
+##################################################
+
+# Keep sudo alive
+#
+sudo echo "" # Asks for passwords
+( while true; do sudo -v; sleep 40; done ) &
+sudoPID=$!
+
+# Clear the dyld Cache
+#
+sudo update_dyld_shared_cache -force
+
+# Invalidate sudo
+sudo -k
